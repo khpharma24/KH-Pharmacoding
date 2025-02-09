b7e5a7e687c9909a0f7ca151350040786a8b6cd6
kh pharma24 <khpharmacoding@gmail.com>
2025-02-09 22:37:34 +0530

    UPDATED

--- notes.js
+++ notes.js
@@ -9,12 +9,17 @@ document.querySelector("form").appendChild(downloadLinkContainer);
 const data = {
     bpharma: {
         1: {
-            "Human Anatomy & Physiology I": {
-                1: "https://example.com/bpharma/sem1/unit1_human_anatomy.pdf",
-                2: "https://example.com/bpharma/sem1/unit2_human_anatomy.pdf",
-                3: "https://example.com/bpharma/sem1/unit3_human_anatomy.pdf",
-                4: "https://example.com/bpharma/sem1/unit4_human_anatomy.pdf",
-                5: "https://example.com/bpharma/sem1/unit5_human_anatomy.pdf"
+            "Pharmaceutics I": {
+                1: "https://drive.google.com/file/d/15mBK36jTa2Enb9gdib90LzfiQzMFoHIi/view?usp=drive_link",
+                prescription:"https://drive.google.com/file/d/1KfofdLevWBYD1x3jD-U5N38v8EOwzk1h/view?usp=drive_link",
+                pharmacopoieas : "https://drive.google.com/file/d/1I6xOCBVByrwXdtoocUj80LROfU9A2rjA/view?usp=drive_link",
+                dosageform: "https://drive.google.com/file/d/1CPgOhygIL-H3O-YG3J5Em984VnJL90Pf/view?usp=drive_link",
+                2: "https://drive.google.com/file/d/15jlU6kuS_OTkvNweOxvzDALEZXOg-7ce/view?usp=drive_link",
+                UNIT2PART2: "https://drive.google.com/file/d/1JR2YIAXyL5pcmBU4_KLJEk540VCEDkGf/view?usp=drive_link",
+                3: "https://drive.google.com/file/d/14ctv-cPMQx8H_G3_45XQ2obbxpMiVWXc/view?usp=drive_link",
+                4: "https://drive.google.com/file/d/1gOho-4-KCFURh8azhsXOuN_bWxgJMVG4/view?usp=drive_link",
+                5: "https://drive.google.com/file/d/17-n_A9JSuOhGJhkI5_pZajY7l-SW8cm7/view?usp=drive_link"
+               
             },
             "Pharmaceutical Analysis": {
                 1: "https://example.com/bpharma/sem1/unit1_pharmaceutical_analysis.pdf",
